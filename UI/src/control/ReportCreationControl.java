e5a9c4e58dbd2ac7a5ac185834162e6c66a7b1bf
erma <eshahu@gmail.com>
2014-04-23 22:49:11 -0400

    Fix ReportReview so that it get's its default values from the ReportCreation form

--- UI/src/control/ReportCreationControl.java
+++ UI/src/control/ReportCreationControl.java
@@ -23,23 +23,36 @@ public class ReportCreationControl extends HttpServlet {
 		try {
 			String reporterFirst = request.getParameter("reporterFirst");
 			String reporterLast = request.getParameter("reporterLast");
+			String reporterPhone = request.getParameter("reporterPhone");
+			String reporterAddr = request.getParameter("reporterAddr");
 			
 			String victimFirst = request.getParameter("victimFirst");
 			String victimLast = request.getParameter("victimLast");
+			String victimPhone = request.getParameter("victimPhone");
+			String victimAddr = request.getParameter("victimAddr");
 			
 			String abuserFirst = request.getParameter("abuserFirst");
 			String abuserLast = request.getParameter("abuserLast");
+			String abuserPhone = request.getParameter("abuserPhone");
+			String abuserAddr = request.getParameter("abuserAddr");
 			
-			AnonymousUser aUser = new AnonymousUser(reporterFirst.concat(reporterLast));
-			
-			String logID = String.valueOf(idCounter.getAndIncrement());
-			
-			AbuseReport aReport = new AbuseReport(logID, abuserFirst.concat(abuserLast), aUser, false);
-			
-			RequestDispatcher disp;
-			disp = getServletContext().getRequestDispatcher("/ReportCreation.jsp");
-		    disp.forward(request, response);
-
+			request.setAttribute("reporterFirst", reporterFirst);
+	        request.setAttribute("reporterLast", reporterLast);
+	        request.setAttribute("reporterPhone", reporterPhone);
+	        request.setAttribute("reporterAddr", reporterAddr);
+	         
+	        request.setAttribute("victimFirst", victimFirst);
+	        request.setAttribute("victimLast", victimLast);
+	        request.setAttribute("victimPhone", victimPhone);
+	        request.setAttribute("victimAddr", victimAddr);
+	         
+	        request.setAttribute("abuserFirst", abuserFirst);
+	        request.setAttribute("abuserLast", abuserLast);
+	        request.setAttribute("abuserPhone", abuserPhone);
+	        request.setAttribute("abuserAddr", abuserAddr);
+	        
+	        getServletContext().getRequestDispatcher("/ReportReview.jsp").forward(request, response);
+   
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
