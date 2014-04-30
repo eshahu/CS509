8e23c0e80c7749ea8e20cbcce1b93d93086e6753
bfloyd <bfloyd@lp-bfloyd-w7.hq.corp.viasat.com>
2014-04-30 19:09:19 -0400

    Fix datetime on audit trail so it is guareenteed correct.

--- UI/src/classes/AuditTrail.java
+++ UI/src/classes/AuditTrail.java
@@ -3,6 +3,7 @@ package classes;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Calendar;
 import java.util.List;
 
 import model.DBUtil;
@@ -23,7 +24,9 @@ public class AuditTrail{
 		boolean b=false;
 		try {
 			conn = DBUtil.getConnection();
-			String sql = String.format("insert into audittrail value(%s)", values);
+			Calendar cal = Calendar.getInstance(); 
+			String sql = String.format("insert into audittrail value(%s,'%s')", values,
+					new java.sql.Timestamp(cal.getTimeInMillis()));
 			pstmt = conn.prepareStatement(sql);
 			int num = pstmt.executeUpdate();
 			if(num==1) {
@@ -42,38 +45,38 @@ public class AuditTrail{
 		if (successful) {
 			message = "User login successful!";
 		}
-		String values = String.format("NULL,'%s', NULL,'%s','%s'",
-				username, message, time);
+		String values = String.format("NULL,'%s', NULL,'%s'",
+				username, message);
 		return track(values);
 	}	
 
 	public boolean trackEditAbuseReport(String username, String reportID, String time){
-		String values = String.format("NULL,'%s', '%s','Abuse report edited!','%s'",
-				username, reportID, time);
+		String values = String.format("NULL,'%s', '%s','Abuse report edited!'",
+				username, reportID);
 		return track(values);
 	}	
 
 	public boolean trackViewAbuseReport(String username, String reportID, String time){
-		String values = String.format("NULL,'%s', '%s','Abuse report viewed!','%s'",
-				username, reportID, time);
+		String values = String.format("NULL,'%s', '%s','Abuse report viewed!'",
+				username, reportID);
 		return track(values);
 	}	
 
 	public boolean trackDownloadAbuseReport(String username, String reportID, String time){
-		String values = String.format("NULL,'%s', '%s','Abuse report downloaded!','%s'",
-				username, reportID, time);
+		String values = String.format("NULL,'%s', '%s','Abuse report downloaded!'",
+				username, reportID);
 		return track(values);
 	}
 
 	public boolean trackFileUploaded(String username, String reportID, String fileName, String time){
-		String values = String.format("NULL,'%s', '%s','File %s uploaded!','%s'",
-				username, reportID, fileName, time);
+		String values = String.format("NULL,'%s', '%s','File %s uploaded!'",
+				username, reportID, fileName);
 		return track(values);
 	}
 
 	public boolean trackFileDownloaded(String username, String reportID, String fileName, String time){
-		String values = String.format("NULL,'%s', '%s','File %s downloaded!','%s'",
-				username, reportID, fileName, time);
+		String values = String.format("NULL,'%s', '%s','File %s downloaded!'",
+				username, reportID, fileName);
 		return track(values);
 	}
 
